a6ad3128cb4bfe205dc577a13fb95ce8ce08e917
RichardY42 <RichardY42@nycstudents.net>
2025-09-15 10:17:22 -0400

    9

---
+++ code.py
@@ -0,0 +1,18 @@
+"""
+x=3
+y=float(3)
+print(x,y)
+
+
+values=[1,2.23,5,7,2,30,15]
+print(values)
+for number in values:
+    print(values[0])
+    print(values[6])
+"""
+
+x="this is a thing"
+y=x.split( )
+z=y[0]
+print(y)
+print(z)
---
+++ notes.py
@@ -0,0 +1,27 @@
+#boolean or true /false
+
+x=true
+y=false
+# == <--evaluate
+if x==true: 
+    print ("x is 10")
+# if you dont use elif it would check both making bugs
+elif x>10:
+    print ("x is greater than 10")
+
+students=["a","b","c","d"]
+print (students[_])
+for student in students:
+    print(student)
+if "c" in students:
+    print("_")
+else:
+    print("_")
+
+for student in students:
+    if student == "c":
+        print("_")
+name="andy"
+print(len(name))
+for letter in name:
+    print(letter)
