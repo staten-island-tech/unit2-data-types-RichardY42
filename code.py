c4c06d8adbdb0b9d19a546e9f26bb900e1a5b2c8
RichardY42 <RichardY42@nycstudents.net>
2025-09-18 09:32:02 -0400

    9/18 931

--- code.py
+++ code.py
@@ -1,15 +1,16 @@
 """
+#intergers and floats:floats represent numbers with decimal values.
 x=3
 y=float(3)
 print(x,y)
 
-
+# lists
 values=[1,2.23,5,7,2,30,15]
 print(values)
 for number in values:
     print(values[0])
     print(values[6])
-
+#string:Strings are a list of characters that represent text.
 count=0
 x=input("input a sentence: ")
 y=x.split( )
@@ -17,3 +18,9 @@ for word in y:
     count+=1
 print(f"You have {count} words in you sentence")
 """
+
+
+#Booleans: we can evaluate statements to True or False never both.These are referred to as boolean data types.
+
+day_of_the_week =input("what day is it? ")
+if day_of_the_week
--- notes.py
+++ notes.py
@@ -1,7 +1,7 @@
 #boolean or true /false
 
-x=true
-y=false
+x=True
+y=False
 # == <--evaluate
 if x==true: 
     print ("x is 10")
@@ -25,3 +25,29 @@ name="andy"
 print(len(name))
 for letter in name:
     print(letter)
+#next week 2 tests 
+#one on 5 data types monday tuesday
+#one on booleans and true tables thursday friday
+
+money = True
+friend = True
+def movie()
+    if money or friend:
+        print ("movies")
+movies()
+
+hw = True
+
+def movies()
+    #"if not" means that if "hw = true" is not true print movie is true and if "hw = true" else print sad true
+    if not hw:
+        print ("movie")
+    else:
+        print("sad")
+movies()
+
+#finding factors
+def faactors(x,y):
+    if x % y == 0
+        print ("factor")
+factors(25,5)
